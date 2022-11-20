5117e287f9e2d4da49b1fca4751b641ec18c4e86
xRaiderx <raider742617@gmail.com>
2022-11-20 13:43:37 +0300

    adminLTE starting

---
+++ resources/js/router.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from "./pages/HomePage";
+import AdminPage from "./pages/AdminPage";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: HomePage, name: "home" },
+        // { path: "/login", component: LoginPage, name: "login" },
+        {
+            path: "/admin",
+            component: AdminPage,
+            name: "admin",
+        },
+        // {
+        //     path: "/admin/users",
+        //     component: UserList,
+        //     name: "admin.users",
+        // },
+        // {
+        //     path: "/admin/users/chat/:id",
+        //     component: UserChat,
+        //     name: "admin.users.chat.id",
+        //     props: true,
+        //     children: [{ path: "edit", component: UserEdit }],
+        // },
+        // {
+        //     path: "/admin/users/register",
+        //     component: RegisterPage,
+        //     name: "register",
+        // },
+        // {
+        //     path: "/chat",
+        //     component: UserPage,
+        //     name: "chat",
+        // },
+        // {
+        //     path: "/:notFound(.*)",
+        //     component: NotFound,
+        //     name: "notfound",
+        // },
+    ],
+});
+
+export default router;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Arizonia&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>

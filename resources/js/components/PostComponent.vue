b78790e82cd31ded56ca5d96437b59eae994314d
xRa1derx <77569202+xRa1derx@users.noreply.github.com>
2022-11-23 17:14:30 +0300

    starting create post section

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,13 +1,29 @@
 <template>
-    <div>Post section</div>
-  </template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Post section</h1>
+        <label for="title" class="form-label">Add a new title</label>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" id="title" />
+        </div>
+        <label for="content">Text</label>
+        <div class="form-floating mb-3">
+          <textarea
+            class="form-control"
+            placeholder="Leave a new content here"
+            id="content"
+            style="height: 100px"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
   
   <script>
-  export default {
-  
-  }
-  </script>
+export default {};
+</script>
   
   <style>
-  
-  </style>
+</style>
